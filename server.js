7c65fb4ff596fe2c4c4d93a39fa5efe1181abed4
Kawika Kekahuna <kawikakekahuna@gmail.com>
2015-07-11 20:10:17 -1000

    updated index.html to show post request

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var HTTP_PAGE_404 = './public/404.html';
 var fs = require('fs');
 var querystring = require('querystring');
 var PORT = 8080;
+var PeriodicElements = {};
 var Method = {
   GET: 'GET',
   POST: 'POST',
@@ -10,6 +11,7 @@ var Method = {
 }
 var PUBLIC_DIR = './public/';
 var ELEMENT_DIR = './public/elements/';
+var ELEMENT_COUNT = 2;
 
 var postDataKey = {
   elementName: '',
@@ -44,6 +46,7 @@ function handleRequest(request, response) {
         });
       }
     });
+
   }
 
   if (request.method === Method.POST) {
@@ -77,7 +80,7 @@ function handleRequest(request, response) {
       var header = createHTMLHeader(postData.elementName);
       var body = createHTMLBody(postData.elementSymbol, postData.elementAtomicNumber, postData.elementDescription);
       var newHTML = createHTML(header, body);
-       console.log('newHTML',newHTML); 
+      console.log('newHTML', newHTML);
       fs.exists(ELEMENT_DIR + postData.filename, function(exists) {
         if (!exists) {
           fs.writeFile(ELEMENT_DIR + postData.filename, newHTML, function(err) {
@@ -86,7 +89,7 @@ function handleRequest(request, response) {
               response.end();
             } else {
               writeFileSuccess(response);
-              renderHomepage(newHTML);
+              renderHomepage(postData);
               response.end();
             }
 
@@ -106,21 +109,26 @@ function handleRequest(request, response) {
  * Create an HTML template that could be easily dynamic.
  * 
  */
-function renderHomepage(newHTML) {
-  var newElement = '<li><a href="/helium.html">Helium</li>';
-  var buffer = new Buffer(newElement);
+function renderHomepage(postData) {
+  var indexHTMLHeader = createIndexHeader();
+  var newPeriodicElement = '<li><a href="/elements/' + postData.filename + '">' + postData.elementName + '</li>';
+  PeriodicElements[postData.filename] = newPeriodicElement;
+  // console.log('PeriodicElements', PeriodicElements);
+  var indexHTMLBody = createIndexBody(newPeriodicElement);
+  var newIndexHTML = indexHTMLHeader + indexHTMLBody;
+  var buffer = new Buffer(newIndexHTML);
   // fs.readFile(PUBLIC_DIR+'index.html',function(err,data){
   //    console.log('data.toString()',data.toString()); 
   // });
 
-  fs.open(PUBLIC_DIR + 'index.html', 'r+', function(err, fd) {
+  fs.open(PUBLIC_DIR + 'index.html', 'w+', function(err, fd) {
 
-    fs.write(fd, buffer, 0, buffer.length, 0, function(err) {
+    fs.write(fd, buffer, 0, buffer.length, null, function(err) {
       if (err) throw err;
 
       fs.close(fd, function() {
         console.log('done writing');
-      })
+      });
     })
 
   });
@@ -157,7 +165,7 @@ function createHTMLHeader(elementName) {
   var header = '<head>\
   <meta charset="UTF-8">\
   <title>The Elements - ' + elementName + '</title>\
-  <link rel="stylesheeeeeeet" href="/css/styles.css">\
+  <link rel="stylesheet" href="/css/styles.css">\
 </head>';
   return header;
 }
@@ -173,20 +181,58 @@ function createHTMLBody(elementName, elementSymbol, elementAtomicNumber, element
   return body;
 }
 
-
-
-function createHTML(header,body) {
+function createHTML(header, body) {
   var generatedHTML = '<!DOCTYPE html>\
   ' + header + '\
   ' + body + '\
 <html lang="en">\
- < /html>\
+ </html>\
   ';
 
   return generatedHTML;
 
 }
 
+function createIndexHeader() {
+  var header = '<!DOCTYPE html>\
+<html lang="en">\
+<head>\
+  <meta charset="UTF-8">\
+  <title>The Elements</title>\
+  <link rel="stylesheet" href="/css/styles.css">\
+</head>\
+<body>\
+  <h1>The Elements</h1>\
+  <h2>These are all the known elements.</h2>\
+  <h3>These are ' + ELEMENT_COUNT++ + '</h3>\
+  <ol>';
+
+  return header;
+
+}
+
+function createIndexBody(newElement) {
+
+  console.log('newElement', newElement);
+
+  var body = '<li>\
+    <a href ="/hydrogen.html"> Hydrogen</a></li>' +
+    '<li><a href="/helium.html"> Helium </a></li>';
+
+
+  var endBody = '</body>\
+</html>';
+
+  var preExistingElements = '';
+  for (var key in PeriodicElements) {
+    preExistingElements += PeriodicElements[key];
+  }
+  console.log('body', body);
+  body = body + preExistingElements + endBody;
+  // console.log('body',body); 
+  return body;
+
+}
 
 
 server.listen(PORT);
